fffa3e23fff4b2211cac74b9661cb0d4940fbb6b
monk <ns708090100@gmail.com>
2026-03-11 15:17:32 +0530

    removed unnecessery comments

--- streamSocketDemo/client.cpp
+++ streamSocketDemo/client.cpp
@@ -6,12 +6,10 @@
 #include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 #define PORT "3490"
 #define MAXDATASIZE 100
 
@@ -30,32 +28,57 @@ int main(int argc, char *argv[]) {
   char s[INET6_ADDRSTRLEN];
 
   if (argc != 2) {
-    std::cout << stderr << "usage : client hostname" << std::endl;
-    exit(0);
+    std::cerr << "usage: client hostname\n";
+    exit(1);
   }
+
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
+
   if ((rv = getaddrinfo(argv[1], PORT, &hints, &res)) != 0) {
-    std::cout << stderr, "getaddrinfo", gai_strerror(rv);
+    std::cerr << "getaddrinfo: " << gai_strerror(rv) << std::endl;
     return 1;
   }
-  for (p = res; p != NULL; p->ai_next) {
+
+  for (p = res; p != NULL; p = p->ai_next) {
+
     if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      perror("client : socketfd");
+      perror("client: socket");
       continue;
     }
+
     inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s,
               sizeof s);
-    std::cout << "client : trying connect to " << s << std::endl;
-    freeaddrinfo(res);
-    if ((numberbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
-      perror("recv");
-      exit(1);
+
+    std::cout << "client: trying to connect to " << s << std::endl;
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("client: connect");
+      continue;
     }
-    buf[numberbytes] = '\0';
-    std::cout << "client : received " << buf;
-    close(sockfd);
-    return 0;
+
+    break;
   }
+
+  if (p == NULL) {
+    std::cerr << "client: failed to connect\n";
+    return 2;
+  }
+
+  freeaddrinfo(res);
+
+  if ((numberbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
+    perror("recv");
+    exit(1);
+  }
+
+  buf[numberbytes] = '\0';
+
+  std::cout << "client: received '" << buf << "'" << std::endl;
+
+  close(sockfd);
+
+  return 0;
 }
--- streamSocketDemo/server.cpp
+++ streamSocketDemo/server.cpp
@@ -1,98 +1,124 @@
 #include <arpa/inet.h>
 #include <cerrno>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
 #include <cstring>
-#include <errno.h>
 #include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
 #define MYPORT "3490"
 #define BACKLOG 10
 
-// void sigchld_handler(int s){
-//     void(s);
-//     // waitpid() might overwrite errno, so we save and restore it:
-//     int saved_errno = errno;
-//     while (waitpid(-1, NULL, WNOHANG)>0);
-//     errno = saved_errno;
-// }
+void sigchld_handler(int s) {
+  (void)s;
+  int saved_errno = errno;
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+    ;
+  errno = saved_errno;
+}
+
 void *get_in_addr(struct sockaddr *sa) {
-  if (sa->sa_family == AF_INET) {
+  if (sa->sa_family == AF_INET)
     return &(((struct sockaddr_in *)sa)->sin_addr);
-  }
+
   return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
+
 int main() {
-  int socketfd, newfd;
-  struct addrinfo hints, *res, *p;
+  int sockfd, newfd;
+  struct addrinfo hints, *servinfo, *p;
   struct sockaddr_storage their_addr;
   socklen_t sin_size;
-  char s[INET6_ADDRSTRLEN];
+  struct sigaction sa;
   int yes = 1;
+  char s[INET6_ADDRSTRLEN];
   int rv;
 
   memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+  hints.ai_family = AF_INET;       // IPv4
+  hints.ai_socktype = SOCK_STREAM; // TCP
+  hints.ai_flags = AI_PASSIVE;     // use my IP
 
-  if (rv = getaddrinfo(NULL, MYPORT, &hints, &res) != 0) {
-    std::cout << stderr << "getaddrinfo " << gai_strerror(rv);
-    return 0;
+  if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+    std::cerr << "getaddrinfo: " << gai_strerror(rv) << std::endl;
+    return 1;
   }
-  for (p = res; p != 0; p->ai_next) {
-    if (socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol) == -1) {
-      perror("server:socket");
+
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("server: socket");
       continue;
     }
 
-    if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) ==
-        -1) {
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
       perror("setsockopt");
       exit(1);
     }
-    if (bind(socketfd, p->ai_addr, p->ai_addrlen) == -1) {
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
       perror("server: bind");
       continue;
     }
+
     break;
   }
-  freeaddrinfo(res);
+
+  freeaddrinfo(servinfo);
+
   if (p == NULL) {
-    std::cout << stderr << "server failed fo bind";
+    std::cerr << "server: failed to bind\n";
+    return 2;
   }
-  if (listen(socketfd, BACKLOG)) {
+
+  if (listen(sockfd, BACKLOG) == -1) {
     perror("listen");
+    exit(1);
   }
-  std::cout << "server waiting for a connection" << std::endl;
+
+  // handle zombie processes
+  sa.sa_handler = sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  std::cout << "server: waiting for connections...\n";
+
   while (true) {
     sin_size = sizeof their_addr;
-    newfd = accept(socketfd, (struct sockaddr *)&their_addr, &sin_size);
+
+    newfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
     if (newfd == -1) {
       perror("accept");
       continue;
     }
+
     inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),
               s, sizeof s);
-    std::cout << "server got connected from " << s << std::endl;
+
+    std::cout << "server: got connection from " << s << std::endl;
+
     if (!fork()) {
-      close(socketfd);
-      if (send(newfd, "Hello, World", 13, 0) == -1) {
+      close(sockfd);
+
+      if (send(newfd, "Hello, world!", 13, 0) == -1)
         perror("send");
-        close(newfd);
-        exit(0);
-      }
+
       close(newfd);
+      exit(0);
     }
+
+    close(newfd);
   }
+
+  return 0;
 }
